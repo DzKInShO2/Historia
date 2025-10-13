3028aff9ac70b04bab626a0af87aafdf1c71da91
zacknuv <zacknuv@proton.me>
2025-10-13 12:01:25 +0800

    tambah kode dasar crawling content external web

--- webserver/static/scripts/archive.js
+++ webserver/static/scripts/archive.js
@@ -1,16 +1,24 @@
+const getUrlContent = async (url) => {
+    try {
+        const response = await fetch("/archive", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ 
+                "web": url
+            })
+        });
+
+        const jason = await response.json();
+
+        return jason;
+    } catch (e) {
+        return null;
+    }
+}
+
 window.onload = async () => {
     const webUrl = new URL(window.location.href).searchParams.get("web");
-
-    fetch("/archive", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ 
-            "web": webUrl
-        })
-    }).then(response => response.text())
-    .then(text => {
-        console.log(text);
-    });
+    console.log(await getUrlContent(webUrl));
 };
--- webserver/webserver.py
+++ webserver/webserver.py
@@ -14,13 +14,33 @@ def is_style(tag):
     is_it = False
 
     if tag.get("rel"):
-        is_it = is_it or tag["rel"] == "stylesheet"
+        is_it = is_it or"stylesheet" in tag["rel"]
 
     if tag.get("type"):
-        is_it = is_it or tag["type"] == "text/css"
+        is_it = is_it and "text/css" in tag["type"]
 
     is_it = is_it and tag.get("href") is not None
     return is_it
+
+def get_external_contents(web, urls):
+    contents = {}
+    for url in urls:
+        try:
+            true_url = url
+            if "https" not in true_url or "http" not in true_url or "ftp" not in true_url:
+                true_url = f"{web}/{url}"
+
+            res = requests.get(true_url)
+            if res.status_code != 200:
+                continue
+            if res.text is None:
+                continue
+
+            contents[true_url] = res.text
+        except Exception as e:
+            continue
+
+    return contents
 
 @app.route("/", methods=["GET"])
 def home_page():
@@ -31,24 +51,47 @@ def archive_page():
     if request.method == "GET":
         return render_template("archive.html")
 
-    res = requests.get(request.json.get("web"))
-    if res.status_code != 200:
-        return "1"
+    content = None
+    try:
+        res = requests.get(request.json.get("web"))
 
-    content = res.text
-    if content is None:
-        return "2"
+        if res.status_code != 200:
+            return "1"
+
+        content = res.text
+        if content is None:
+            return "2"
+    except Exception as e:
+        print(f"EXCEPTION: {e}")
+        return "3"
 
     soup = BeautifulSoup(content, "html.parser")
 
     # Filter SCRIPT with srcs
+
+    # Head external resources
     script_urls = list(filter(is_script, soup.head.find_all('script')))
-    style_urls = list(filter(is_style, soup.head.find_all('link')))
+    style_urls  = list(filter(is_style, soup.head.find_all('link')))
 
-    script_urls = [ tag["src"] for tag in scripts ]
-    style_urls = [ tag["href"] for tag in styles ]
+    # Body external resources
+    script_urls = [*script_urls, *list(filter(is_script, soup.body.find_all('script')))]
+    style_urls  = [*style_urls, *list(filter(is_style, soup.body.find_all('style')))]
+    img_urls    = soup.body.find_all('img')
 
-    return f"{styles}\n{scripts}"
+    script_urls = [ tag["src"] for tag in script_urls ]
+    style_urls  = [ tag["href"] for tag in style_urls ]
+    img_urls    = [ tag["src"] for tag in img_urls ]
+
+    web_url = request.json.get("web")
+    response = {
+        "head": str(soup.head).replace("<head>", "").replace("</head>", "").strip(),
+        "body": str(soup.body).replace("<body>", "").replace("</body>", "").strip(),
+        "scripts": get_external_contents(web_url, script_urls),
+        "styles": get_external_contents(web_url, style_urls),
+        "images": get_external_contents(web_url, img_urls)
+    }
+
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run()
